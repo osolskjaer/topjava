b03104f32470956c7567182c962268a15180bbb1
osolskjaer <oleg-trophymenko@yandex.ru>
2017-12-22 15:10:36 +0200

    HW5 Task 5 - HSQLDB profile for save repository method added

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHsqldbImpl.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import javax.sql.DataSource;
+
+@Profile(Profiles.HSQL_DB)
+@Repository
+public class JdbcMealRepositoryHsqldbImpl extends JdbcMealRepositoryImpl {
+    public JdbcMealRepositoryHsqldbImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(dataSource, jdbcTemplate, namedParameterJdbcTemplate);
+    }
+}
--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,19 +11,27 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Repository
+@Profile(Profiles.POSTGRES_DB)
 public class JdbcMealRepositoryImpl implements MealRepository {
 
-    private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+    @Autowired
+    protected Environment environment;
 
-    private final JdbcTemplate jdbcTemplate;
+    protected static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    protected final JdbcTemplate jdbcTemplate;
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
@@ -37,14 +47,43 @@ public class JdbcMealRepositoryImpl implements MealRepository {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
-    @Override
-    public Meal save(Meal meal, int userId) {
-        MapSqlParameterSource map = new MapSqlParameterSource()
-                .addValue("id", meal.getId())
+    private String getProfileName()
+    {
+        if(Arrays.stream(environment.getActiveProfiles()).anyMatch(
+                env -> (env.equalsIgnoreCase(Profiles.HSQL_DB)
+                )))
+        {
+            return Profiles.HSQL_DB;
+        }
+        else if(Arrays.stream(environment.getActiveProfiles()).anyMatch(
+                env -> (env.equalsIgnoreCase(Profiles.POSTGRES_DB)
+                )))
+        {
+            return Profiles.POSTGRES_DB;
+        }
+        return "";
+    }
+    private MapSqlParameterSource getSaveParametersMap(Meal meal, int userId)
+    {
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        if(getProfileName().equalsIgnoreCase(Profiles.HSQL_DB))
+        {
+            map.addValue("date_time", Date.from(meal.getDateTime().atZone(ZoneId.systemDefault()).toInstant()));
+        }
+        else if(getProfileName().equalsIgnoreCase(Profiles.POSTGRES_DB))
+        {
+            map.addValue("date_time", meal.getDateTime());
+        }
+        map.addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
                 .addValue("user_id", userId);
+        return map;
+    }
+    @Override
+    public Meal save(Meal meal, int userId) {
+
+        MapSqlParameterSource map = getSaveParametersMap(meal, userId);
 
         if (meal.isNew()) {
             Number newId = insertMeal.executeAndReturnKey(map);
@@ -81,8 +120,17 @@ public class JdbcMealRepositoryImpl implements MealRepository {
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDate, endDate);
+        if (getProfileName().equalsIgnoreCase(Profiles.HSQL_DB))
+        {
+            return jdbcTemplate.query(
+                    "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
+                    ROW_MAPPER, userId, Date.from(startDate.atZone(ZoneId.systemDefault()).toInstant()), Date.from(endDate.atZone(ZoneId.systemDefault()).toInstant()));
+        }
+        else
+        {
+            return jdbcTemplate.query(
+                    "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
+                    ROW_MAPPER, userId, startDate, endDate);
+        }
     }
 }
